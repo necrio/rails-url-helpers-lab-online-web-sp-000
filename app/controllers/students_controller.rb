a8eac6b9571696c471c80bc06d8817d96861d9c4
Keith Isham <kisham8899@gmail.com>
2019-12-04 12:07:03 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -18,7 +18,7 @@ class StudentsController < ApplicationController
     redirect_to student_path(@student)
   end 
   
-    private
+   private
 
     def set_student
       @student = Student.find(params[:id])
